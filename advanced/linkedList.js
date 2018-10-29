beadd3b67e7b01238dd37e4053337b11aa4a5329
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-10-29 11:23:07 -0400

    worked on linked list

--- advanced/linkedList.js
+++ advanced/linkedList.js
@@ -5,28 +5,64 @@ class Node {
   }
 }
 
-function countNodes(head) {
-  //assuming at least 1 node
-  let count = 1;
-  let current = head;
-  while (current.next !== null) {
-    current = current.next;
-    count += 1;
+class LinkedList {
+  constructor(node) {
+    if (node.constructor === Node) {
+      this.node = node;
+    } else {
+      this.node = new Node();
+    }
   }
-  return count;
+
+  addNode(value) {
+    let current = this.node;
+    while (current.next !== null) {
+      current = current.next;
+    }
+
+    current.next = new Node(value);
+  }
+
+  deleteNode(value) {
+    let current = this.node;
+    if (current.data === value) {
+      current = current.next;
+      this.node = current;
+      return;
+    }
+    while (current.next !== null) {
+      if (current.next.data === value) {
+        current.next = current.next.next;
+      }
+      if (current.next) {
+        current = current.next;
+      } else {
+        break;
+      }
+    }
+  }
+
+  countNodes() {
+    //assuming at least 1 node
+    let count = 1;
+    let current = this.node;
+    while (current.next !== null) {
+      current = current.next;
+      count += 1;
+    }
+    return count;
+  }
 }
 
 let nodeA = new Node(6);
-let nodeB = new Node(3);
-let nodeC = new Node(4);
-let nodeD = new Node(2);
-let nodeE = new Node(1);
-
-nodeA.next = nodeB;
-nodeB.next = nodeC;
-nodeC.next = nodeD;
-nodeD.next = nodeE;
 
 console.log("This linked list's length is: (should print 5)");
 
-console.log(countNodes(nodeA));
+let linkedList = new LinkedList(nodeA);
+console.log(linkedList.countNodes());
+linkedList.addNode(7);
+linkedList.addNode(8);
+linkedList.deleteNode(6);
+linkedList.deleteNode(8);
+console.log(linkedList.countNodes());
+console.log(linkedList);
