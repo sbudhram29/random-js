e40345bd2d12cb9053218103958bed1826e38a7d
Sean Budhram <sbudhram@carlisle-etcetera.com>
2019-05-02 09:35:49 -0400

    testing flat

--- learning/flat.js
+++ learning/flat.js
@@ -1,19 +1,10 @@
-const arr1 = [1, 2, 3, [1, 2, 3, 4, [2, 3, 4]]];
+const arr = [1, 2, 3, [1, 2, 3, 4, [2, 3, 4]]];
 
-function flattenDeep(arr1) {
-  return arr1.reduce(
-    (acc, val) => {
-      if (Array.isArray(val)) {
-        return acc.concat(flattenDeep(val));
-      } else {
-        return acc.concat(val);
-      }
-    },
+const flat = (arr) => [].concat(...arr);
 
-    []
-  );
-}
-const flattenArr = flattenDeep(arr1); // [1, 2, 3, 1, 2, 3, 4, 2, 3, 4]
-const flattenArr2 = flattenDeep([1, 2, 3, [1, 2, 3, [3, 2, 1]]]); // [1, 2, 3, 1, 2, 3, 4, 2, 3, 4]
+const flatDeep = (arr) =>
+  arr.reduce((res, curr) => {
+    return Array.isArray(curr) ? res.concat(flatDeep(curr)) : res.concat(curr);
+  }, []);
 
-console.log(flattenArr2);
+console.log(flatDeep(arr));
